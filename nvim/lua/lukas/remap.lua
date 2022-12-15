3c76de812283591e16aa2dc51cfb9d57238f0d88
Lukas Taroza <mintyone920@gmail.com>
2022-12-15 09:51:33 +0200

    added nvim remaps for tab navigation

--- nvim/lua/lukas/remap.lua
+++ nvim/lua/lukas/remap.lua
@@ -3,10 +3,18 @@ local keymap = require("lukas.keymap")
 keymap.nnoremap("<S-Right>", "gt")
 keymap.nnoremap("<S-Left>", "gT")
 
+keymap.nnoremap("L", "gt")
+keymap.nnoremap("H", "gT")
+
 keymap.nnoremap("<C-Right>", "<C-w>l")
 keymap.nnoremap("<C-Up>", "<C-w>k")
 keymap.nnoremap("<C-Down>", "<C-w>j")
 keymap.nnoremap("<C-Left>", "<C-w>h")
+
+keymap.nnoremap("<C-l>", "<C-w>l")
+keymap.nnoremap("<C-k>", "<C-w>k")
+keymap.nnoremap("<C-j>", "<C-w>j")
+keymap.nnoremap("<C-h>", "<C-w>h")
 
 keymap.nnoremap("<leader>e", "<cmd>Ex<cr>")
 keymap.nnoremap("<leader>w", "<cmd>w<cr>")
@@ -15,7 +23,3 @@ keymap.nnoremap("<leader>q", "<cmd>q<cr>")
 keymap.nnoremap("<leader>p", "\"+p")
 keymap.vnoremap("<leader>y", "\"+y")
 
-keymap.nnoremap("<C-l>", "<C-w>l")
-keymap.nnoremap("<C-k>", "<C-w>k")
-keymap.nnoremap("<C-j>", "<C-w>j")
-keymap.nnoremap("<C-h>", "<C-w>h")
